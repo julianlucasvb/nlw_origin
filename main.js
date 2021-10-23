3820ebe64afbbc7344e7a38c1d7aa833d5c1540a
Julian Lucas <julianlucasvb@gmail.com>
2021-10-23 14:02:22 -0300

    projeto finalizado

---
+++ main.js
@@ -0,0 +1,31 @@
+const linksSocialMedia = {
+  github: 'julianlucasvb',
+  youtube: 'UCrPEneR4H7oZzeeDkXQJP1w',
+  facebook: 'julianlucasvb',
+  instagram: 'julianlucasvb',
+  twitter: 'julianlucasvb'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+getGitHubProfileInfos()
